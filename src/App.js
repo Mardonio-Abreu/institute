bd3b70f9c0426b17d13516f17b3511398665be36
Armando <armando@localhost.localdomain>
2023-08-23 10:31:53 -0600

    PreEntrega1Abreu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import BasicNavbar from "./components/NavBar/NavBarMain";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+  return (   
+    <BasicNavbar />
+  )
+  }
 export default App;
---
+++ src/components/Cart/CartWidget.js
@@ -0,0 +1,13 @@
+
+function Cart (){
+    return (
+        <>
+            <img src="cartIcon.png" alt="Shopping cart"/>
+            <span>5</span>
+        </>
+        
+        )
+}
+
+export default Cart;
+
